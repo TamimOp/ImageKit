3a568dbd7f7b8b99c8a760b8641ea3b7e15de878
TamimOp <tamimshadanik@gmail.com>
2025-08-21 00:03:23 +0600

    Refactor User model to use named imports for mongoose and ensure proper schema definition

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 import bcrypt from "bcryptjs";
 
 export interface IUser {
@@ -9,7 +9,7 @@ export interface IUser {
   updatedAt?: Date;
 }
 
-const userSchema = new mongoose.Schema<IUser>(
+const userSchema = new Schema<IUser>(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -26,4 +26,9 @@ userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
+  next();
 });
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;

ad4335698779e86fab042f5fbba7dea322443d79
TamimOp <tamimshadanik@gmail.com>
2025-08-31 12:48:34 +0600

    Refactor authentication setup to define handler and enhance credentials provider logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 import { authOption } from "@/lib/auth";
 import NextAuth from "next-auth";
 
-NextAuth(authOption);
+const handler = NextAuth(authOption);
+
+export { handler as GET, handler as POST };
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,53 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+// import GithubProvider from "next-auth/providers/github";
 
 export const authOption: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID!,
+    //   clientSecret: process.env.GITHUB_SECRET!,
+    // }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "jsmith@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid email or password");
+        }
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("User not found");
+          }
+          //   const isValid = await user.comparePassword(credentials.password);
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isValid) {
+            throw new Error("Invalid email or password");
+          }
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          console.log("auth error", error);
+          throw error;
+        }
+      },
     }),
   ],
 };
